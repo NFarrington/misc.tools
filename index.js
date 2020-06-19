3f8820be707ec3436162ff00918b0a139cbb5298
Neil Farrington <me@neilfarrington.com>
2020-06-19 20:11:04 +0100

    Switch to ES module format

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var fs = require('fs');
-var handlebars = require('handlebars');
-var layouts = require('handlebars-layouts');
+import { readFileSync, existsSync, mkdirSync, writeFileSync } from 'fs';
+import handlebars from 'handlebars';
+import layouts from 'handlebars-layouts';
 
 // Register helpers
 handlebars.registerHelper(layouts(handlebars));
 
 // Register partials
-handlebars.registerPartial('base-layout', fs.readFileSync('src/layouts/base.hbs', 'utf8'));
+handlebars.registerPartial('base-layout', readFileSync('src/layouts/base.hbs', 'utf8'));
 
 // Compile templates
-var index = handlebars.compile(fs.readFileSync('src/index.hbs', 'utf8'));
-var pwgen = handlebars.compile(fs.readFileSync('src/pwgen.hbs', 'utf8'));
-var errorPage = handlebars.compile(fs.readFileSync('src/error.hbs', 'utf8'));
+var index = handlebars.compile(readFileSync('src/index.hbs', 'utf8'));
+var pwgen = handlebars.compile(readFileSync('src/pwgen.hbs', 'utf8'));
+var errorPage = handlebars.compile(readFileSync('src/error.hbs', 'utf8'));
 
 // Render templates
-if (!fs.existsSync('build/')) {
-    fs.mkdirSync('build');
+if (!existsSync('build/')) {
+    mkdirSync('build');
 }
-fs.writeFileSync('build/index.html', index({'pageId': 'index', 'index-active': 'active'}));
-fs.writeFileSync('build/pwgen.html', pwgen({'pwgen-active': 'active'}));
-fs.writeFileSync('build/error.html', errorPage());
+writeFileSync('build/index.html', index({ 'pageId': 'index', 'index-active': 'active' }));
+writeFileSync('build/pwgen.html', pwgen({ 'pwgen-active': 'active' }));
+writeFileSync('build/error.html', errorPage());
