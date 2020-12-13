307e196857c8a6d3f4a36f1e91f27b02554bb664
Neil Farrington <me@neilfarrington.com>
2020-12-13 23:40:43 +0000

    Deploy via GitHub Actions

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eufo pipefail
-
-node index.js
-
-aws s3 cp build/ s3://misc.tools/ --recursive
-aws s3api put-bucket-website --bucket misc.tools --website-configuration file://site.json
-aws s3api put-bucket-website --bucket www.misc.tools --website-configuration file://www-redirect.json
